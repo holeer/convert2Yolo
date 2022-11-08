808fcaeefb6cc336cebfc9b5c890abcffcec12b9
holer <9085595+holeer@user.noreply.gitee.com>
2022-11-08 22:34:21 +0800

    更新 run.ps1
    删除 manifest.txt

--- run.ps1
+++ run.ps1
@@ -1,5 +1,5 @@
-conda create --name 2yolo --file requirements.txt
+# conda create --name 2yolo --file requirements.txt
 d:
 cd D:\Workspace\lhy-ship\convert2Yolo
 conda activate 2yolo
-python example.py --datasets VOC --img_path ./unaugmented/train/images/ --label ./unaugmented/train/anns/ --convert_output_path ./unaugmented/train/labels/ --img_type ".jpg" --manifest_path ./unaugmented/train/ --cls_list_file ./unaugmented/train/names.txt
+python example.py --datasets VOC --img_path ../yolov7/unaugmented/images/val/ --label ../yolov7/unaugmented/anns/val/ --convert_output_path ../yolov7/unaugmented/labels/val/ --img_type ".jpg" --manifest_path ../yolov7/unaugmented/ --cls_list_file ../yolov7/unaugmented/names.txt
