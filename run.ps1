eb005d586d626982c844997ecef2f98ac74c4554
holer <9085595+holeer@user.noreply.gitee.com>
2022-11-10 21:39:49 +0800

    更新 run.ps1

--- run.ps1
+++ run.ps1
@@ -1,5 +1,6 @@
 # conda create --name 2yolo --file requirements.txt
 d:
-cd D:\Workspace\lhy-ship\convert2Yolo
+# cd D:\Workspace\lhy-ship\convert2Yolo
+cd convert2Yolo
 conda activate 2yolo
-python example.py --datasets VOC --img_path ../yolov7/unaugmented/images/val/ --label ../yolov7/unaugmented/anns/val/ --convert_output_path ../yolov7/unaugmented/labels/val/ --img_type ".jpg" --manifest_path ../yolov7/unaugmented/ --cls_list_file ../yolov7/unaugmented/names.txt
+python example.py --datasets VOC --img_path ../yolov7/generated-dataset/images/ --label ../yolov7/generated-dataset/anns/ --convert_output_path ../yolov7/generated-dataset/labels --img_type ".jpg" --manifest_path ../yolov7/generated-dataset/ --cls_list_file ../yolov7/generated-dataset/names.txt
